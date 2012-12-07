26f8c323f36751d56ad5bbae150318ba05facf8d
Lau Ching Jun <lauchingjun@hotmail.com>
2012-12-07 15:33:26 +0800

    Add comments explaining backtrack fix on deflist regex

--- js-markdown-extra.js
+++ js-markdown-extra.js
@@ -2444,6 +2444,11 @@ MarkdownExtra_Parser.prototype.doDefLists = function(text) {
           '('                     + // $2
             '[ ]{0,' + less_than_tab + '}' +
             '((?:[ \\t]*\\S.*\\n)+)' + // $3 = defined term
+                                       // [porting note] Original regex from PHP is
+                                       // (?>.*\S.*\n), which matches a line with at
+                                       // least one non-space character. Change the
+                                       // first .* to [ \t]* stops unneccessary
+                                       // backtracking hence improves performance
             '\\n?'                +
             '[ ]{0,' + less_than_tab + '}:[ ]+' + // colon starting definition
           ')'                     +
