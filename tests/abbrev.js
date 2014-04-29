ac715328e03a49441388398dd7fde95dc31bd476
Hisateru Tanaka <tanakahisateru@gmail.com>
2014-04-30 01:23:52 +0900

    #33 Test c38635cb5a

--- tests/abbrev.js
+++ tests/abbrev.js
@@ -23,3 +23,21 @@ QUnit.test( "01 - To retain spaces", function() {
     var result = Markdown(input);
     QUnit.assert.equal(result, expected);
 });
+
+
+/**
+ * Test 02 - Use twice
+ */
+QUnit.test( "02 - Use twice", function() {
+    var input =
+        "*[CMS]: Content Management System" + "\n" +
+        "*[HTML]: Hyper Text Markup Language" + "\n" +
+        "\n" +
+        "CMS generates HTML pages." + "\n";
+
+    var expected =
+        '<p><abbr title="Content Management System">CMS</abbr> generates <abbr title="Hyper Text Markup Language">HTML</abbr> pages.</p>' + "\n";
+
+    var result = Markdown(input);
+    QUnit.assert.equal(result, expected);
+});
